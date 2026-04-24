4b288fcce9041189edeb943c0811360a1dcede08
ihza <ihza@ipds.bps.go.id>
2026-04-24 21:17:23 +0700

    fix: update webmanifest paths to resolve 404 errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
+        if (config('app.env') !== 'local' || str_starts_with(config('app.url'), 'https://')) {
             URL::forceScheme('https');
         }
 
